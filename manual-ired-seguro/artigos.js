3986e8d4b0b7bd5f0bf9e5f6bbc21e039b44e785
biellaraa <gdias1414@gmail.com>
2025-06-14 10:33:26 -0300

    adicao: adicionado artigos e atualizado o estilo

---
+++ manual-ired-seguro/artigos.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    console.log("Página de Atividades carregada.");
+
+    // A lógica para a busca, visualização de artigos detalhados,
+    // e os quizzes interativos serão implementados aqui nas próximas etapas.
+
+});
